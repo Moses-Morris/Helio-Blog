3c7f940cab21a07bd525ad9a6d2664a79cd88d93
Moses-Morris <mosesmorrisdev@gmail.com>
2025-11-06 20:01:24 +0300

    Creating the authentication inflask: Used the flask_login library.

--- website/__init__.py
+++ website/__init__.py
@@ -12,7 +12,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['secret_key'] = "blogdaily"
+    app.config['secret_key'] = "30876adf7888fd2e3c50d787fa26d32b8cf488087502f85588f1ab84063cdc9a"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' #configure our database
     db.init_app(app) #initialize our db with our flask app
     #Get rid of this since you are going to use a blueprint for this
